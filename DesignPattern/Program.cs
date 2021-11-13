2193ce1fe0f8d2b8191814aba2e92b28167823ac
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2021-11-13 17:46:47 +0900

    :sparkles: DesignPatternプロジェクトを新規作成

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
